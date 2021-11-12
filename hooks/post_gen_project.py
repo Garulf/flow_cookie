f2490812b996f5f54b61e9b51afe43b389e6e9fe
Garulf <535299+Garulf@users.noreply.github.com>
2021-11-12 09:04:00 -0500

    Add plugin file helper functions

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,6 +6,18 @@ import subprocess
 import uuid
 import json
 
+PLUGIN_FILE = "plugin.json"
+PLUGIN_PATH = os.path.join(os.getcwd(), PLUGIN_FILE)
+
+def read_manifest():
+    with open(PLUGIN_PATH, "r") as f:
+        return json.load(f)
+    
+def write_manifest(data):
+    with open(PLUGIN_PATH, "w") as f:
+        json.dump(content, f, indent=4)
+    return data
+
 def run_cmd(cmd):
     process = subprocess.Popen(cmd)
     process.wait()
@@ -20,21 +32,27 @@ def clone_repo(repo, dir):
 def generate_uuid():
     return str(uuid.uuid4()).replace('-', '').upper()
 
-def add_uuid():
-    path = os.path.join(os.getcwd(), "plugin.json")
-    with open(path, 'r') as f:
-        plugin = json.load(f)
-    plugin['ID'] = generate_uuid()
-    with open(path, 'w') as f:
-        f.write(json.dumps(plugin, indent=4))
+def add_uuid(plugin_manifest):
+    plugin_manifest['ID'] = generate_uuid()
+    write_manifest(plugin_manifest)
     # print(f"Changed version to: {plugin['Version']}")
+    
+def update_description(desc):
+    with open(os.path.join(os.getcwd(), ".git", "description"), "w") as f:
+        f.write(desc)
 
-clone_repo("https://github.com/Garulf/flow_commands", ["bin"])
+if __name__ == "__main__":
+    plugin_manifest = read_manifest()
+    clone_repo("https://github.com/Garulf/flow_commands", ["bin"])
 
-clone_repo("https://github.com/Garulf/flow_workflows", [".github", "workflows"])
+    clone_repo("https://github.com/Garulf/flow_workflows", [".github", "workflows"])
 
-add_uuid()
+    add_uuid(plugin_manifest)
 
-run_cmd(["pip", "install", "--target=./lib", "-r", "requirements.txt"])
+    run_cmd(["pip", "install", "--target=./lib", "-r", "requirements.txt"])
 
-run_cmd(["git", "init])
+    run_cmd(["git", "init])
+         
+    update_description(plugin_manifest["Description"])
+         
+
